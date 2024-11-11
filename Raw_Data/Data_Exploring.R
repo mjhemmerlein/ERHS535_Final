78f268f2f5ed431768c8cf99b3c2493902e40268
mjhemmerlein <megan.hemmerlein@colostate.edu>
2024-11-11 10:18:52 -0700

    setting up folders

---
+++ Raw_Data/Data_Exploring.R
@@ -0,0 +1,9 @@
+library(readr)
+library(tidyverse)
+
+most_visited_nps_species_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-08/most_visited_nps_species_data.csv')
+
+Seasonality <- most_visited_nps_species_data %>%
+  filter(NPSTags == "Noxious")
+
+
