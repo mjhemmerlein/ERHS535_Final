2c807315afd056cccf0fe1d73aa40e0818986a65
Victoria Talbott <vtalbott7124@gmail.com>
2024-11-20 09:32:12 -0700

    added comments for steps

--- R_Scripts/Elevation_Data.R
+++ R_Scripts/Elevation_Data.R
@@ -3,3 +3,13 @@ library(tigris)
 install.packages("terra")
 
 list.files("Raw_Data/gmted2010/")
+
+
+# what are these files?
+
+# how do we use these files?
+
+# how do we overlay this with the national park coordinates
+  # find national park coordinates
+
+# devide elevation and line it up with the elevation range from data
